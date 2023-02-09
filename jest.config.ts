579ebe0c8765f60c90f48fa5b5a92fd0f9bf38cd
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2023-02-09 16:17:24 -0500

    jest comfig added

---
+++ jest.config.ts
@@ -0,0 +1,36 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  verbose: true,
+  automock: false,
+  clearMocks: true,
+  coverageProvider: 'v8',
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
+  roots: ['<rootDir>/app/tests'],
+  testMatch: ['**/tests/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  forceExit: true,
+  detectOpenHandles: true,
+  modulePathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/app/tests/setup.ts'],
+  setupFilesAfterEnv: ['./app/setup.ts'],
+  moduleNameMapper: {
+    '@controller/(.*)': ['<rootDir>/app/controller/$1'],
+    '@interfaces/(.*)': ['<rootDir>/app/interfaces/$1'],
+    '@models/(.*)': ['<rootDir>/app/models/$1'],
+    '@routes/(.*)': ['<rootDir>/app/routes/$1'],
+    '@services/(.*)': ['<rootDir>/app/services/$1'],
+    '@external-services/(.*)': ['<rootDir>/app/services/external/$1'],
+    '@tests/(.*)': ['<rootDir>/app/tests/$1'],
+    '@utils/(.*)': ['<rootDir>/app/utils/$1'],
+    '@root/(.*)': ['<rootDir>/$1'],
+  }
+};
+
+export default config;
+
+// https://swizec.com/blog/how-to-configure-jest-with-typescript/
+// for reference on how to import fn in your test files
